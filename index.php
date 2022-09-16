ead07a9a129a9b0e2a073f9365ea180610de3297
saputra21z <hendisaputra201@gmail.com>
2022-09-16 10:19:27 +0800

    uload pertama

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <table border="1"  width="850px" >
+        <tr>
+            <td ><center><b>E-Ticket / <i>E-Tiket</i></b></center></td>
+            <td colspan="4" rowspan="2"><center><img src="logo-traveloka.png" alt="ini gambar logo" height="50px" width="200px" srcset=""></center></td>
+        </tr>
+        <tr>
+            <td width="300px"><b>Departrue Flight / <i>Penerbangan Landas</i></b></td>
+        </tr>
+        <tr>
+            <td colspan="3" width="350px" height="50px"><b>Lion Air</b></td>
+            <td >Minggu, 11 September 2022</td>
+            <td>Boxing Code</td>
+        </tr>
+        <tr>
+            <td rowspan="2" colspan="3" width="350px">Sublass M (Economy)</td>
+            <td>
+                <ul>
+                    <li>15.30</li>
+                    <li>Balikpapan</li>
+                </ul>
+            </td>
+            <td rowspan="2">L1234JP</td>
+        </tr>
+        <tr>
+            <td width="200px">
+                <ul>
+                    <li>18.25</li>
+                    <li>Jakarta</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="5">
+               <p>Hal-hal yang perlu diperhatikan:</p> 
+               <ul>
+                    <li>Tunjukan e-tiket dan identitas penumpang saat check-in</li>
+                    <li>Check-in paling lambat 90 menit sebelum keberangkatan</li>
+                    <li>Waktu yang tertera adalah waktu bandara setempat</li>
+                </ul>
+            </td>
+        </tr>
+        <tr>
+            <td>No</td>
+            <td width="150px">Passenger(s)</td>
+            <td colspan="2">Ticket Type</td>
+            <td>Baggage</td>
+        </tr>
+        <tr>
+            <td><i>No</i></td>
+            <td width="100px"><i>Nama Penumpang</i></td>
+            <td colspan="2"><i>Jenis Tiket</i></td>
+            <td><i>Bagasi</i></td>
+        </tr>
+        <tr>
+            <td height="50px">1.</td>
+            <td  width="150px">Hendy Saputra</td>
+            <td colspan="2">
+            <input type="radio" name="gender" id="">Dewasa<br>
+            <input type="radio" name="gender" id="">Bayi<br>
+            </td>
+            <td>
+                <center><input type="number" name="badan" id="berat" value="0" min="0" max="10"> kg</center>
+            </td>
+            <!-- <td rowspan="2" colspan="2">Ticket Type</td>
+            <td rowspan="2">Baggage</td> -->
+        </tr>
+        <tr>
+            <td colspan="5"><center><b>Nama Pemesan</b></center></td>
+        </tr>
+        <tr>
+            <td rowspan="4" colspan="3" width="350px">
+                <br>
+                Nama <br>
+                <br>
+                NIM <br>
+                <br>
+                No Telp <br>
+                <br>
+                Praktikum Web
+                <br>
+            </td>
+            <td rowspan="2" colspan="3" width="350px">
+                <br>
+                Hendy Saputra <br>
+                <br>
+                2109106120 <br>
+                <br>
+                083153568054 <br>
+                <br>
+                <a href="https://github.com/saputra21z?tab=repositories">Postest 1 Web</a>
+                <br>
+            </td>
+        </tr>
+   </table>
+
+   <!-- <form action="">
+    Nama: <br>
+    <input type="text" name="" id=""> <br>
+    Umur: <br>
+    <input type="number" min="0" name="" id=""> <br>
+    Tanggal: <br>
+    <input type="date" name="" id=""> <br>
+    gender: <br>
+    <input type="radio" name="gender" id="">pria<br>
+    <input type="radio" name="gender" id="">wanita<br>
+    Hobi: <br>
+    <input type="checkbox" name="hobi" id="">pria<br>
+    <input type="checkbox" name="hobi" id="">wanita<br>
+   </form> -->
+
+</body>
+</html>
